2adcc8a38efe44d2aed85e55cbcc13f0588dbfa4
Richard S <richardsit@gmail.com>
2017-07-26 22:18:37 +0200

    Add shopping-list component dummy data

--- recipe-book-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipes/recipe.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2015/03/24/00/28/cuisine-686905_960_720.jpg')
+  ];
 
   constructor() { }
 
