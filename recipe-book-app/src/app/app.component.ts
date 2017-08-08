936fca0e6ca640b34333e607395fa68b300e7184
Richard S <richardsit@gmail.com>
2017-08-08 09:07:58 +0200

    Test navigation and inter-component communication

--- recipe-book-app/src/app/app.component.ts
+++ recipe-book-app/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ricci"s app works!';
+  loadedFeature = 'recipe';
+
+  onFeatureSelected(selectedFeature: string) {
+    this.loadedFeature = selectedFeature;
+  }
 }
