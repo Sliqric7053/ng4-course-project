40062b58953b8f8b8e150d3f020222514e185396
Richard S <richardsit@gmail.com>
2017-08-25 15:28:08 +0200

    Add recipe service

--- recipe-book-app/src/app/recipes/recipes.component.ts
+++ recipe-book-app/src/app/recipes/recipes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 
+import { RecipeService } from './recipe.service';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -10,13 +12,14 @@ import { Recipe } from './recipe.model';
 export class RecipesComponent implements OnInit {
 selectedItem: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected
+    .subscribe(
+      (recipe: Recipe) => {
+        this.selectedItem = recipe;
+      }
+    );
   }
-
-  onSelectedRecipeItem(eventData: Recipe) {
-    this.selectedItem = eventData;
-  }
-
 }
