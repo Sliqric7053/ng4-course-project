c27840b107005ed2ddf399bd69461c29017d1740
Richard S <richardsit@gmail.com>
2017-09-10 14:29:22 +0200

    Implement onDestroy lifecycle hook to unsubscribe from Subject

--- recipe-book-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { Recipe } from '../recipes/recipe.model';
-
 import { ShoppingListService } from './shopping-list.service';
 
 @Component({
@@ -10,18 +9,23 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientChanged
+    this.subscription = this.shoppingListService.ingredientChanged
     .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
         );
+      }
+
+      ngOnDestroy() {
+        this.subscription.unsubscribe();
       }
   }
