cbabadbbbaea08461cd62d5a42807cddd71f3f2c
Richard S <richardsit@gmail.com>
2017-09-10 14:27:38 +0200

    Replace event emitter with Subject

--- recipe-book-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs/Rx';
 
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-    ingredientChanged = new EventEmitter<Ingredient[]>();
+    ingredientChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -17,7 +17,12 @@ export class ShoppingListService {
 
       addIngredient(newIngredient: Ingredient) {
         this.ingredients.push(newIngredient);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
+      }
+
+      addIngredients(ingredients: Ingredient[]) {
+        this.ingredients.push(...ingredients);
+        this.ingredientChanged.next(this.ingredients.slice());
       }
 
     }
