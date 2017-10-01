cd0d0c864b2c0814cfdb95a518f9d58fbddafe9a
Richard S <richardsit@gmail.com>
2017-10-01 16:03:08 +0200

    Implement save and fetch data functionality using firebase

--- recipe-book-app/src/app/header/header.component.ts
+++ recipe-book-app/src/app/header/header.component.ts
@@ -1,9 +1,30 @@
+import { Response } from '@angular/http';
 import { Component } from '@angular/core';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+
+import { Recipe } from 'app/recipes/recipe.model';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class  HeaderComponent {
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {}
 
+    onSave() {
+        this.dataStorageService.storeRecipes()
+        .subscribe(
+            (response: Response) => {
+                const data = response.json();
+                console.log(data);
+            },
+        (error) => console.log(error)
+        );
+    }
+
+    onFetch() {
+        this.dataStorageService.fetchRecipes();
+    }
 }
