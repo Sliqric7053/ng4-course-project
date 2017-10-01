36e23e51cc476fd8d98d250f351268ed3f292aea
Richard S <richardsit@gmail.com>
2017-10-01 21:34:10 +0200

    Remove redundat code

--- recipe-book-app/src/app/shared/data-storage.service.ts
+++ recipe-book-app/src/app/shared/data-storage.service.ts
@@ -9,19 +9,17 @@ export class DataStorageService {
     constructor(private http: Http, private recipeService: RecipeService) {}
 
     storeRecipes() {
-        //  const headers = new Headers({'Content-Type': 'application/json'});
-         return this.http.put('https://ng4-recipe-book-3d9d9.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+         return this.http.put('https://ng4-recipe-book-3d9d9.firebaseio.com/store-recipes.json', this.recipeService.getRecipes());
     }
 
     fetchRecipes() {
-         this.http.get('https://ng4-recipe-book-3d9d9.firebaseio.com/recipes.json')
+         this.http.get('https://ng4-recipe-book-3d9d9.firebaseio.com/get-recipes.json')
          .map(
             (response: Response) => {
                 const recipeData: Recipe[] = response.json();
                 for (const recipe of recipeData) {
                     if (!recipe['ingredients']) {
                         recipe['ingredients'] = [];
-                        console.log(recipe);
                     }
                 }
                 return recipeData;
