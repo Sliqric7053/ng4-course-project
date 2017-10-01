d2ee16dcab5217e583db25a2fa56e1c902638d5a
Richard S <richardsit@gmail.com>
2017-10-01 21:35:58 +0200

    Change let to const

--- recipe-book-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -64,7 +64,7 @@ export class RecipeEditComponent implements OnInit {
      let recipeName = '';
      let recipeImagePath = '';
      let recipeDescription = '';
-     let recipeIngredients = new FormArray([]);
+     const recipeIngredients = new FormArray([]);
 
      if (this.editMode) {
        const recipe = this.recipeService.getRecipe(this.id);
@@ -73,7 +73,7 @@ export class RecipeEditComponent implements OnInit {
        recipeDescription = recipe.description;
 
        if (recipe['ingredients']) {
-         for (let ingredient of recipe.ingredients) {
+         for (const ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
               'name': new FormControl(ingredient.name, Validators.required),
