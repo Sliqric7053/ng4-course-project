132cb753907b6db83e7281756d581ae7110d7f2f
Richard S <richardsit@gmail.com>
2017-10-07 15:48:58 +0200

    Beautfy syntax

--- recipe-book-app/src/app/auth/signup/signup.component.ts
+++ recipe-book-app/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
-import { AuthService } from '../auth.service';
-import { NgForm } from '@angular/forms/src/directives';
 import { Component, OnInit } from '@angular/core';
+
+import { NgForm } from '@angular/forms/src/directives';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
