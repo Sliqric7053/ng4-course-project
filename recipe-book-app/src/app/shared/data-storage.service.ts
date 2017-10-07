4528ba06c8abeb8d0371ed18c72436f6a93c0f9d
Richard S <richardsit@gmail.com>
2017-10-07 15:46:33 +0200

    Add token to data storage service

--- recipe-book-app/src/app/shared/data-storage.service.ts
+++ recipe-book-app/src/app/shared/data-storage.service.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '../auth/auth.service';
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
 
@@ -6,14 +7,20 @@ import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {}
+    constructor(private http: Http,
+        private recipeService: RecipeService,
+        private authService: AuthService) {}
 
     storeRecipes() {
-         return this.http.put('https://ng4-recipe-book-3d9d9.firebaseio.com/store-recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        // tslint:disable-next-line:max-line-length
+        return this.http.put('https://ng4-recipe-book-3d9d9.firebaseio.com/store-recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     fetchRecipes() {
-         this.http.get('https://ng4-recipe-book-3d9d9.firebaseio.com/get-recipes.json')
+        const token = this.authService.getToken();
+
+         this.http.get('https://ng4-recipe-book-3d9d9.firebaseio.com/get-recipes.json?auth=' + token)
          .map(
             (response: Response) => {
                 const recipeData: Recipe[] = response.json();
