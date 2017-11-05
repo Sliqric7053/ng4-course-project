c905066d8ddabd722a96ace8a8290ae9a4577bdb
Richard S <richardsit@gmail.com>
2017-11-05 06:32:04 +0200

    Add auth and homepage components

--- recipe-book-app/src/app/app.module.ts
+++ recipe-book-app/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { RegisterModule } from './auth/register.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
@@ -6,11 +5,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-import { RecipesModule } from './recipes/recipes.module';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 import { Recipe } from './recipes/recipe.model';
 
@@ -20,11 +20,11 @@ import { AuthService } from './auth/auth.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AuthGuard } from 'app/auth/auth-guard.service';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,9 +32,8 @@ import { AuthGuard } from 'app/auth/auth-guard.service';
     FormsModule,
     AppRoutingModule,
     SharedModule,
-    RecipesModule,
     ShoppingListModule,
-    RegisterModule
+    AuthModule,
   ],
   providers: [RecipeService, ShoppingListService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
