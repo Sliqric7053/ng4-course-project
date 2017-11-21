97bd5c6e1496aee27ff2aaeee06eca02a3bb316c
Richard S <richardsit@gmail.com>
2017-11-21 08:22:13 +0200

    Setup first reducer and fix dependency issues (typescript and rxjs)

--- recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,31 +1,31 @@
-// import { Ingredient } from '../../shared/ingredient.model';
+import { Ingredient } from '../../shared/ingredient.model';
 
-// import * as ShoppingListActions from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
-// export interface AppState {
-//     shoppingList: State;
-//   }
+export interface AppState {
+    shoppingList: State;
+  }
 
-// export interface State {
-// ingredients: Ingredient[];
-// }
+export interface State {
+ingredients: Ingredient[];
+}
 
-// const initialState: State = {
-//     ingredients: [
-//         new Ingredient('Apples', 5),
-//         new Ingredient('Carrots', 5),
-//         new Ingredient('Potatoes', 7)
-//       ]
-// };
+const initialState: State = {
+    ingredients: [
+        new Ingredient('Apples', 5),
+        new Ingredient('Carrots', 5),
+        new Ingredient('Potatoes', 7)
+      ]
+};
 
-// export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
-// switch (action.type) {
-//     case ShoppingListActions.ADD_INGREDIENT:
-//         return {
-//             ...state,
-//             ingredients: [...state.ingredients, action.payload]
-//         };
-//     default:
-//         return state;
-//     }
-// }
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+        return {
+            ...state,
+            ingredients: [...state.ingredients, action.payload]
+        };
+    default:
+        return state;
+    }
+}
