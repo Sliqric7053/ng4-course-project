9d966e5f1af380ca419be0a16c0c10023f7c3379
Richard S <richardsit@gmail.com>
2017-11-22 09:36:24 +0200

    Add more reducer functions

--- recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
+++ recipe-book-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -25,6 +25,31 @@ switch (action.type) {
             ...state,
             ingredients: [...state.ingredients, action.payload]
         };
+    case ShoppingListActions.ADD_INGREDIENTS:
+        return {
+            ...state,
+            ingredients: [...state.ingredients, ...action.payload]
+        };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+    const ingredient = state.ingredients[action.payload.index];
+    console.log('state.ingredients[action.payload.index]', ingredient);
+    const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+    };
+    const ingredients = [...state.ingredients];
+    ingredients[action.payload.index] = updatedIngredient;
+        return {
+            ...state,
+            ingredients: ingredients
+        };
+    case ShoppingListActions.DELETE_INGREDIENT:
+    const oldIngredients = [...state.ingredients];
+    oldIngredients.splice(action.payload, 1);
+        return {
+            ...state,
+            ingredients: oldIngredients
+        };
     default:
         return state;
     }
