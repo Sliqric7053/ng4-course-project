f0d6ffeaba8899eb39c8bb755f73f5c07a07ceef
Richard S <richardsit@gmail.com>
2017-12-11 07:47:56 +0200

    Update http interceptor to use store

--- recipe-book-app/src/app/shared/auth.interceptor.ts
+++ recipe-book-app/src/app/shared/auth.interceptor.ts
@@ -4,15 +4,21 @@ import { Observable } from 'rxjs/Observable';
 import { AuthService } from 'app/auth/auth.service';
 import { Injectable } from '@angular/core';
 
+import * as fromApp from '../app-store/app.reducers';
+import * as fromAuth from '../auth/auth-store/auth.reducer';
+import { Store } from '@ngrx/store';
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-constructor(private authService: AuthService) {}
+constructor(private store: Store<fromApp.AppState>) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('intercepted', req);
-        const copiedReq = req.clone({
-            params: req.params.set('auth', this.authService.getToken()
-        )});
-        return next.handle(copiedReq);
+        return this.store.select('userAuth')
+        .switchMap((authState: fromAuth.State) => {
+            const copiedReq = req.clone({
+                params: req.params.set('auth', authState.token)});
+                return next.handle(copiedReq);
+        });
     }
 }
