80f14d9b2fad0d903eaca292fc5c8e5758109057
Richard S <richardsit@gmail.com>
2017-12-11 07:47:24 +0200

    Update auth guard to use store

--- recipe-book-app/src/app/auth/auth-guard.service.ts
+++ recipe-book-app/src/app/auth/auth-guard.service.ts
@@ -1,12 +1,17 @@
-import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 
+import { Store } from '@ngrx/store';
+import * as fromApp from '../app-store/app.reducers';
+import * as fromAuth from '../auth/auth-store/auth.reducer';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService) { }
+    constructor(private store: Store<fromApp.AppState>) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.authService.isAuthenticated();
+        return this.store.select('userAuth').map((authState: fromAuth.State) => {
+            return authState.authenticated;
+        });
     }
 }
