add699f02dfe0807637ad4af380d108e52716458
Richard S <richardsit@gmail.com>
2017-12-11 07:49:28 +0200

    Refactor auth service to use ngrx

--- recipe-book-app/src/app/auth/auth.service.ts
+++ recipe-book-app/src/app/auth/auth.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Store } from '@ngrx/store';
+
 import * as firebase from 'firebase';
+
+import * as fromApp from '../app-store/app.reducers';
+import * as AuthActions from '../auth/auth-store/auth.actions';
 
 @Injectable()
 export class AuthService {
-    token: string;
+    // token: string;
 
-    constructor(private router: Router) { }
+    constructor(
+        private router: Router,
+        private store: Store<fromApp.AppState>
+        ) { }
 
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(
+            user => {
+                this.store.dispatch(new AuthActions.SignUp());
+            }
+        )
         .catch(
             (error) => console.log(error)
         );
@@ -18,7 +31,7 @@ export class AuthService {
 
     logOut() {
         firebase.auth().signOut();
-        this.token = null;
+        this.store.dispatch(new AuthActions.LogOut());
         this.router.navigate(['/']);
     }
 
@@ -26,10 +39,13 @@ export class AuthService {
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(
             (response) => {
+                this.store.dispatch(new AuthActions.SignIn());
                 this.router.navigate(['/']);
                 firebase.auth().currentUser.getIdToken()
                 .then(
-                    (token: string) => this.token = token
+                    (token: string) => {
+                        this.store.dispatch(new AuthActions.SetToken(token));
+                    }
                 );
             }
         )
@@ -38,15 +54,15 @@ export class AuthService {
         );
     }
 
-    getToken() {
-        firebase.auth().currentUser.getIdToken()
-        .then(
-            (token: string) => this.token = token
-        );
-        return this.token;
-    }
+    // getToken() {
+    //     firebase.auth().currentUser.getIdToken()
+    //     .then(
+    //         (token: string) => this.token = token
+    //     );
+    //     return this.token;
+    // }
 
-    isAuthenticated() {
-        return this.token != null;
-    }
+    // isAuthenticated() {
+    //     return this.token != null;
+    // }
 }
