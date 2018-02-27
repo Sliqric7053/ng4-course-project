9cd13e1c80e7fe9b5e5b68ce20858947aa828c75
Sliqric7053 <richardsit@gmail.com>
2018-02-27 09:07:09 +0200

    Add try signIn function

--- recipe-book-app/src/app/auth/auth-store/auth.effects.ts
+++ recipe-book-app/src/app/auth/auth-store/auth.effects.ts
@@ -1,71 +1,155 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import { Actions, Effect } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/mergeMap';
+import { fromPromise } from 'rxjs/observable/fromPromise';
 import * as firebase from 'firebase';
 
-import * as AuthActions from '../auth-store/auth.actions';
-import { fromPromise } from 'rxjs/observable/fromPromise';
-import { Router } from '@angular/router';
+import * as AuthActions from './auth.actions';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthEffects {
-
-    @Effect()
-    authSignup = this.actions$
-        .ofType(AuthActions.TRY_SIGNUP)
-        .map((action: AuthActions.TrySignUp) => {
-            return action.payload;
-        })
-        .switchMap((authData: {username: string, password: string}) => {
-            return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
-        })
-        .switchMap(() => {
-            return fromPromise(firebase.auth().currentUser.getIdToken());
-        })
-        .mergeMap((token: string) => {
-            this.router.navigate(['/']);
-            return [
-                {
-                    type: AuthActions.SIGN_UP
-                },
-                {
-                    type: AuthActions.SET_TOKEN,
-                    payload: token
-                }
-            ];
-        });
-
-    @Effect()
-    authSignin = this.actions$
-    .ofType(AuthActions.TRY_SIGNIN)
-    .map((action: AuthActions.TrySignIn) => {
-        return action.payload;
+  @Effect()
+  authSignup = this.actions$
+    .ofType(AuthActions.TRY_SIGNUP)
+    .map((action: AuthActions.TrySignUp) => {
+      return action.payload;
     })
-    .switchMap((authData: {username: string, password: string}) => {
-        return fromPromise(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+    .switchMap((authData: { username: string, password: string }) => {
+      return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
     })
     .switchMap(() => {
-        return fromPromise(firebase.auth().currentUser.getIdToken());
+      return fromPromise(firebase.auth().currentUser.getIdToken());
     })
     .mergeMap((token: string) => {
-        this.router.navigate(['/']);
-        return [
-            {
-                type: AuthActions.SIGN_IN
-            },
-            {
-                type: AuthActions.SET_TOKEN,
-                payload: token
-            }
-        ];
+      return [
+        {
+          type: AuthActions.SIGN_UP
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
     });
 
-    @Effect({dispatch: false})
-    authLogOut = this.actions$
-    .ofType(AuthActions.LOG_OUT)
+  @Effect()
+  authSignin = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .map((action: AuthActions.TrySignUp) => {
+      return action.payload;
+    })
+    .switchMap((authData: { username: string, password: string }) => {
+      return fromPromise(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));      // .catch(error => {
+      //   console.log('errrrr', error)
+      //   throw error;
+      // });
+      // .catch(error => Observable.of(new AuthActions.SignInErrorAction({ error: error })));
+      // .catch(error => Observable.of(console.log('errrrrror', error)));
+    })
     .switchMap(() => {
-        this.router.navigate(['/']);
-        return fromPromise(firebase.auth().signOut());
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      this.router.navigate(['/']);
+      return [
+        {
+          type: AuthActions.SIGN_IN
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
     });
 
-    constructor(private actions$: Actions, private router: Router) {}
-};
+  @Effect({ dispatch: false })
+  authLogout = this.actions$
+    .ofType(AuthActions.LOG_OUT)
+    .do(() => {
+      this.router.navigate(['/']);
+    });
+
+  constructor(private actions$: Actions, private router: Router) {
+  }
+}
+
+
+
+
+
+// import { Injectable } from '@angular/core';
+// import { Effect, Actions } from '@ngrx/effects';
+// import * as firebase from 'firebase';
+
+// import * as AuthActions from '../auth-store/auth.actions';
+// import { fromPromise } from 'rxjs/observable/fromPromise';
+// import { Router } from '@angular/router';
+
+// @Injectable()
+// export class AuthEffects {
+
+//     @Effect()
+//     authSignup = this.actions$
+//         .ofType(AuthActions.TRY_SIGNUP)
+//         .map((action: AuthActions.TrySignUp) => {
+//             return action.payload;
+//         })
+//         .switchMap((authData: {username: string, password: string}) => {
+//             return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+//         })
+//         .switchMap(() => {
+//             return fromPromise(firebase.auth().currentUser.getIdToken());
+//         })
+//         .mergeMap((token: string) => {
+//             this.router.navigate(['/']);
+//             return [
+//                 {
+//                     type: AuthActions.SIGN_UP
+//                 },
+//                 {
+//                     type: AuthActions.SET_TOKEN,
+//                     payload: token
+//                 }
+//             ];
+//         });
+
+//     @Effect()
+//     authSignin = this.actions$
+//     .ofType(AuthActions.TRY_SIGNIN)
+//     .map((action: AuthActions.TrySignIn) => {
+//         return action.payload;
+//     })
+//     .switchMap((authData: {username: string, password: string}) => {
+//         return fromPromise(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+//     })
+//     .switchMap(() => {
+//         return fromPromise(firebase.auth().currentUser.getIdToken());
+//     })
+//     .mergeMap((token: string) => {
+//         this.router.navigate(['/']);
+//         return [
+//             {
+//                 type: AuthActions.SIGN_IN
+//             },
+//             {
+//                 type: AuthActions.SET_TOKEN,
+//                 payload: token
+//             }
+//         ];
+//     });
+
+//     @Effect({dispatch: false})
+//     authLogOut = this.actions$
+//     .ofType(AuthActions.LOG_OUT)
+//     .switchMap(() => {
+//         this.router.navigate(['/']);
+//         return fromPromise(firebase.auth().signOut());
+//     });
+
+//     constructor(private actions$: Actions, private router: Router) {}
+// };
--- recipe-book-app/src/app/auth/signin/signin.component.ts
+++ recipe-book-app/src/app/auth/signin/signin.component.ts
@@ -5,6 +5,7 @@ import { NgForm } from '@angular/forms/src/directives';
 
 import * as fromApp from '../../app-store/app.reducers';
 import * as AuthActions from '../auth-store/auth.actions';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -13,7 +14,9 @@ import * as AuthActions from '../auth-store/auth.actions';
 })
 export class SigninComponent implements OnInit {
 
-  constructor(private store: Store<fromApp.AppState>) { }
+  constructor(private store: Store<fromApp.AppState>,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
@@ -21,7 +24,13 @@ export class SigninComponent implements OnInit {
   onSignIn(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
-    this.store.dispatch(new AuthActions.TrySignIn({username: email, password: password}));
-  }
 
+    this.store.dispatch(new AuthActions.TrySignIn({ username: email, password: password }));
+
+    this.store.select('userAuth').subscribe(user => {
+      if (user.authenticated) {
+        this.router.navigate(['../'], { relativeTo: this.route });
+      }
+    });
+  }
 }
